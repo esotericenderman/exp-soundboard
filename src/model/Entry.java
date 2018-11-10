26b52aaa26d7db666c1bfcc8907c1d9e7d588591
Michael Brown <michaelmbcubed@gmail.com>
2018-11-09 19:08:50 -0500

    first commit
    model and testers added

---
+++ src/model/Entry.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Entry {
+	
+	//private File soundFile;
+	private int[] keyNumbers;
+
+	public Entry() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/model/Soundboard.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Soundboard {
+	
+	private List<Entry> entries;
+
+	public Soundboard() {
+		entries = new ArrayList<Entry>();
+	}
+
+}
